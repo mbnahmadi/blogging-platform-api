e88c5816672d98f5f4ff80b2376caa24b166d521
mbnahmadi <mbn.ahmadi78@gmail.com>
2024-10-19 17:16:03 +0330

    insert PostView

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,5 +5,6 @@ from .models import PostsModel
 class PostSerializer(serializers.ModelSerializer):
     class Meta:
         model = PostsModel
+        fields = '__all__'
 
     # def validate(self,data):
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,20 @@
 from django.shortcuts import render
 from .models import posts_collection
+from .serializers import PostSerializer
+from rest_framework.response import Response
+from rest_framework.views import APIView
+from rest_framework import status  
 # Create your views here.
+
+
+class PostView(APIView):
+    def post(self,request):
+        serializer = PostSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({'message':'post created successfuly!'},status=status.HTTP_201_CREATED)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+
 
 
 
